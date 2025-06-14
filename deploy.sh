82655db7babfae23628e1c976ae15cf3ee37bbc5
Akash041 <55125707+Akash041@users.noreply.github.com>
2025-06-14 22:41:57 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-docker run -d -p 80:80 akashadmin07/react-app-dev
+set -e
+
+if [ "$BRANCH_NAME" == "main" ]; then
+    IMAGE="akashadmin07/react-app-prod:latest"
+else
+    IMAGE="akashadmin07/react-app-dev:latest"
+fi
+
+# Remove previous container if running
+docker rm -f react-container || true
+
+# Run the new container
+docker run -d -p 80:80 --name react-container $IMAGE
+
 
